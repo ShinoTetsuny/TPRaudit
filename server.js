fd3aa6d42747f5710c994e5f96550f0d5ae1384d
Tom <tom.ollivier01@gmail.com>
2023-12-20 17:12:41 +0100

    Login and register Work and genere a Token

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 let cors = require("cors")
+
+//Models
 const sequelize = require('./database/database');
 const Option = require('./models/option')
 const Engine = require('./models/engine')
@@ -9,16 +11,22 @@ const User = require('./models/user')
 const Purchase = require('./models/purchase')
 const Model = require('./models/model')
 
+//Routes
+const Authentication = require('./routes/authentication')
+
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/authentication", Authentication)
 
-let maj = async(req,res)=>{
+
+/*let maj = async(req,res)=>{
     await sequelize.sync({force: true});
     console.log('maj effectuée');
 }
-maj();     
+maj();*/
+    
 
 app.listen(3000, () => {
     console.log("serverStart")
