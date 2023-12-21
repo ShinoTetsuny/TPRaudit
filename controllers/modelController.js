26780ba2eafbe98b34977533c4232450e39396ea
William Mollet <william.mollet.en.uvsq.fr>
2023-12-21 10:47:21 +0100

    Prise en compte de modelOption

--- controllers/modelController.js
+++ controllers/modelController.js
@@ -9,27 +9,41 @@ exports.getAllModels = (req, res, next) => {
         .catch((error) => res.status(400).json({ error }))
 };
 
-exports.getOneModel = (req, res, next) => {
-    model.findOne({ where: { id: req.params.id } })
-        .then((model) => res.status(200).json(model))
+exports.getOneModel = async (req, res, next) => {
+    let object = {}
+
+    let aModel = await model.findOne({ where: { id: req.params.id } })
         .catch((error) => res.status(400).json({ error }))
+    object.model = aModel
+
+    let options = await model.findOne({ where: { id: req.params.id } })
+        .then((model) => model.getOptions())
+        .catch((error) => res.status(400).json({ error }))
+    object.options = options
+
+    res.status(200).json(object)
 };
 
 exports.createModel = async (req, res, next) => {
 
-    // create model
-    await model.create({
+    let newModel = await model.create({
         name: req.body.name,
         seat: req.body.seat,
         door: req.body.door,
         price: req.body.price,
         engineId: req.body.engineId,
     })
-        .then((model) => res.status(200).json(model))
-        .catch((error) => res.status(400).json({ error }))
+    .catch((error) => res.status(400).json({ error }))
+    req.body.options.forEach(option => {
+        newModel.addOption(option)
+    });
+
+    res.status(201).json(newModel)
 };
 
 exports.editModel = (req, res, next) => {
+    let aModel = model.findOne({ where: { id: req.params.id } })
+        .catch((error) => res.status(400).json({ error }))
     model.update({
         name: req.body.name,
         seat: req.body.seat,
@@ -37,11 +51,20 @@ exports.editModel = (req, res, next) => {
         price: req.body.price,
         engineId: req.body.engineId,
     }, { where: { id: req.params.id } })
+        .then(() => {
+            aModel.removeOptions()
+            req.body.options.forEach(option => {
+                aModel.addOption(option)
+            });
+        })
         .then((model) => res.status(200).json(model))
         .catch((error) => res.status(400).json({ error }))
 };
 
 exports.deleteModel = (req, res, next) => {
+    let aModel = model.findOne({ where: { id: req.params.id } })
+        .catch((error) => res.status(400).json({ error }))
+    aModel.removeOptions()
     model.destroy({ where: { id: req.params.id } })
         .then((model) => res.status(200).json(model))
         .catch((error) => res.status(400).json({ error }))
--- controllers/optionController.js
+++ controllers/optionController.js
@@ -1,36 +1,62 @@
-const option = require('../models/option')
+const option = require('../models/option');
 
 exports.getAllOptions = (req, res, next) => {
     option.findAll()
-        .then((option) => res.status(200).json(option))
+        .then((option) => res.status(200).json(option, models))
         .catch((error) => res.status(400).json({ error }))
 };
 
-exports.getOneOption = (req, res, next) => {
-    option.findOne({ where: { id: req.params.id } })
-        .then((option) => res.status(200).json(option))
+exports.getOneOption = async (req, res, next) => {
+    let object = {}
+    let anOption = await option.findPk( req.params.id )
         .catch((error) => res.status(400).json({ error }))
+        
+    object.option = anOption
+
+    let models = await option.findPk( req.params.id )
+        .then((option) => option.getModels())
+        .catch((error) => res.status(400).json({ error }))
+
+    object.models = models
+
+    res.status(200).json(object)
 };
 
-exports.createOption = (req, res, next) => {
-    option.create({
+exports.createOption = async (req, res, next) => {
+    let newOption = await option.create({
         name: req.body.name,
         price: req.body.price,
     })
-        .then((option) => res.status(200).json(option))
         .catch((error) => res.status(400).json({ error }))
+    req.body.models.forEach(model => {
+        newOption.addModel(model)
+    });
+
+    res.status(201).json(newOption)
 };
 
-exports.editOption = (req, res, next) => {
-    option.update({
+exports.editOption = async (req, res, next) => {
+    let oldOption = await option.findPk( req.params.id )
+        .catch((error) => res.status(400).json({ error }))
+    
+    await option.update({
         name: req.body.name,
         price: req.body.price,
     }, { where: { id: req.params.id } })
+        .then(() => {
+            oldOption.removeModels()
+            req.body.models.forEach(model => {
+                oldOption.addModel(model)
+            });
+        })
         .then((option) => res.status(200).json(option))
         .catch((error) => res.status(400).json({ error }))
 };
 
-exports.deleteOption = (req, res, next) => {
+exports.deleteOption = async (req, res, next) => {
+    let oldOption = await option.findOne({ where: { id: req.params.id } })
+        .catch((error) => res.status(400).json({ error }))
+    oldOption.removeModels()
     option.destroy({ where: { id: req.params.id } })
         .then((option) => res.status(200).json(option))
         .catch((error) => res.status(400).json({ error }))
