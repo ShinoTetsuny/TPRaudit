f987f37d4f0499eb507a3b15a7772c9369d93017
Andre <andre.coelho@hotmail.fr>
2023-12-22 21:50:41 +0100

    update purchases

--- views/src/components/purchases.js
+++ views/src/components/purchases.js
@@ -2,32 +2,41 @@ import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 
-/*
-L'ajout d'options ne fonctionne pas, l'option ne s'affiche pas et le prix n'est pas mise à jour
-*/
+/* const jwt = require('jsonwebtoken');
+const token = response.data.token; */
 
-/*
-Le bouton Acheter ajoute une ligne dans la table purchase avec l'option selectionné mais l'id User n'est pas inséré
-*/
+// Décodez le token
+// const decodedToken = jwt.decode(token);
+
+// Vérifiez si le décodage a réussi et si l'ID de l'utilisateur est présent
+/* try {
+    // Vérifiez et décodez le token
+    const decodedToken = jwt.verify(token, JWT_SECRET); // Remplacez 'votre_cle_secrete' par votre clé secrète réelle
+  
+    // L'extraction de l'ID de l'utilisateur dépend de la structure du token
+    const userId = decodedToken.userId;
+    console.log('ID de l\'utilisateur:', userId);
+    // Utilisez l'ID de l'utilisateur comme nécessaire
+  } catch (error) {
+    console.error('Erreur lors de la vérification du token:', error.message);
+  } */
+
 const PurchasePage = () => {
     const { id } = useParams();
     const [modelData, setModelData] = useState(null);
-    const [optionsList, setOptionsList] = useState([]);
+    // const [optionsList, setOptionsList] = useState([]);
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
+
     const apiUrl = 'http://localhost:3000/model/';
     const optionsApiUrl = 'http://localhost:3000/option/';
 
     const fetchModelDetails = async () => {
         try {
-            const response = await axios.get(`${apiUrl}${id}`);
+            const response = await axios.get(`http://localhost:3000/model/${id}`);
             setModelData(response.data);
-
-            const optionsResponse = await axios.get(`${optionsApiUrl}`);
-            setOptionsList(optionsResponse.data);
-
-            // Update totalPrice with the initial price
-            setTotalPrice(response.data.model.price);
+    
+            // setTotalPrice(response.data.model.price);
         } catch (error) {
             console.error(error);
         }
@@ -37,9 +46,9 @@ const PurchasePage = () => {
         fetchModelDetails();
     }, [id]);
 
-    // Ceci ne fonctionne pas
     const { model } = modelData || {};
-    const updateTotalPrice = useCallback(() => {
+
+    /* const updateTotalPrice = useCallback(() => {
         const optionsPrice = selectedOptions.reduce((acc, optionId) => {
             const option = optionsList.find(opt => opt.id === optionId);
             return acc + (option ? option.price : 0);
@@ -49,21 +58,22 @@ const PurchasePage = () => {
     }, [selectedOptions, optionsList, model]);
 
     useEffect(() => {
-        // Call the updateTotalPrice function whenever selectedOptions or optionsList change
         updateTotalPrice();
-    }, [updateTotalPrice]);
+    }, [updateTotalPrice]); */
 
-    const handleAddOption = () => {
-        // Update selectedOptions
-        setSelectedOptions(Array.from(document.querySelector('select').selectedOptions, (option) => option.value));
-    };
+/*     const handleAddOption = () => {
+        const selectedOptions = Array.from(document.querySelector('select').selectedOptions, ({ value }) => value);
+        setSelectedOptions(selectedOptions);
+        updateTotalPrice();
+    }; */
 
     const handlePurchase = async () => {
         try {
             const { userId } = model || {};
             const price = totalPrice || 0;
     
-            const response = await axios.post('http://localhost:3000/purchase', {
+            // Envoyez la requête d'achat avec les détails à votre API
+            const response = await axios.post('http://localhost:3000/purchase/purchase', {
                 modelId: id,
                 userId,
                 price,
@@ -71,15 +81,15 @@ const PurchasePage = () => {
             });
     
             if (response.status === 201) {
-                alert('Purchase successful!');
+                alert('Achat réussi !');
             } else {
-                console.error('Error', response.status);
+                console.error('Erreur', response.status);
             }
         } catch (error) {
             console.error('Erreur lors de l\'achat', error);
-            // Handle error (show error message, etc.)
         }
     };
+    
 
     return (
         <div className="purchase-page-container">
@@ -103,22 +113,17 @@ const PurchasePage = () => {
                         setSelectedOptions(Array.from(e.target.selectedOptions, (option) => option.value));
                     }}
                 >
-                    {optionsList.map((option) => (
+                    {modelData?.options.map((option) => (
                         <option key={option.id} value={option.id}>
                             {option.name}
                         </option>
                     ))}
                 </select>
-                <button className="go-back-link" onClick={handleAddOption}>
-                    Ajouter
-                </button>
-            </div>
-            <div>
-                <p>Prix total: {totalPrice}</p>
-                <p>Options sélectionnées: {selectedOptions.map(optionId => optionsList.find(opt => opt.id === optionId)?.name).join(', ')}</p>
             </div>
 
-            <button className="go-back-link" onClick={handlePurchase}>Acheter</button>
+            <button className="go-back-link" onClick={handlePurchase}>
+                Acheter
+            </button>
 
             <Link to={`/model/${id}`} className="go-back-link">
                 Retour aux détails du modèle
