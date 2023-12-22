8d1843600616da37276d0b12f5a0ea4e25a75d7d
Andre <andre.coelho@hotmail.fr>
2023-12-22 12:44:00 +0100

    update modeldetails

--- views/src/components/modeldetails.js
+++ views/src/components/modeldetails.js
@@ -3,32 +3,43 @@ import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 
 const apiUrl = 'http://localhost:3000/model/';
+const engineApiUrl = 'http://localhost:3000/engine/';
 
 const ModelDetails = () => {
     const { id } = useParams();
     const [modelData, setModelData] = useState(null);
+    const [engineData, setEngineData] = useState(null);
+  
+    const fetchModelDetails = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}${id}`);
+            setModelData(response.data);
+
+            const engineResponse = await axios.get(`${engineApiUrl}${response.data.model.engineId}`);
+            setEngineData(engineResponse.data);
+
+            // console.log(response.data)
+            // console.log(engineResponse.data)
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
-        const fetchModelDetails = async () => {
-            try {
-                const response = await axios.get(`${apiUrl}${id}`);
-                setModelData(response.data);
-                console.log(response.data)
-            } catch (error) {
-                console.error(error);
-            }
-        };
         fetchModelDetails();
     }, [id]);
 
-    if (!modelData) {
-        return <div>Loading...</div>;
-    }
-
     // console.log(modelData);
 
-    const { model, options } = modelData;
+    if (!modelData || !engineData) {
+        return <p>Loading...</p>;
+    }
 
+    const { model } = modelData;
+    // const { engine } = engineData;
+    let lesOptions = modelData.options;
+    console.log(engineData.name)
+    // console.log(modelData.options);
     return (
         <div className="model-details-container">
             <h1 className="model-details-title">Model Details</h1>
@@ -37,17 +48,20 @@ const ModelDetails = () => {
             <p className="model-details-item">Seat: {model.seat}</p>
             <p className="model-details-item">Door: {model.door}</p>
             <p className="model-details-item">Price: {model.price}</p>
-            <p className="model-details-item">Engine ID: {model.engineId}</p>
-            {/* Display other details of the model as needed */}
-            
-            <h2 className="model-details-subtitle">Options</h2>
-            {options.map((option) => (
-                <div key={option.id} className="model-option-container">
-                    <p className="model-details-item">Option ID: {option.id}</p>
-                    {/* Display other details of the option as needed */}
-        </div>
-    ))}
-        
+            <p className="model-details-item">Engine Name: {engineData.name}</p>
+
+            {lesOptions && lesOptions.length > 0 && (
+                <div>
+                    <h2 className="model-details-option"> Options </h2>
+                    {lesOptions.map((option) => (
+                        <div key={option.name} className="model-details-option">
+                            <p className="model-details-item">Name: {option.name}</p>
+                            <p className="model-details-item">Price: {option.price}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+
         <Link to="/" className="go-back-link">Go back to homepage</Link>
     </div>
     );
